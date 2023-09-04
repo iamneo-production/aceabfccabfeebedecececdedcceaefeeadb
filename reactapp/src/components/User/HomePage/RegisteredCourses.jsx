a90582f0fc657b14005c5f76a1592dc5aa30b550
Iamneo <techadmin@iamneo.ai>
2023-09-04 11:55:25 +0000

    Backup Commit

--- reactapp/src/components/User/HomePage/RegisteredCourses.jsx
+++ reactapp/src/components/User/HomePage/RegisteredCourses.jsx
@@ -1,15 +1,53 @@
-import React from 'react'
-import {useParams} from 'react-router-dom'
-import UserAppBar from '../../UserAppBar'
+import React from 'react';
+import { useParams } from 'react-router-dom';
+import UserAppBar from '../../UserAppBar';
+import { Card, CardContent, Typography, Button } from '@mui/material';
 
 const RegisteredCourses = () => {
-  const params =  useParams()
+  const registeredCourse = [
+    {
+      courseId: 1,
+      courseName: "Masters in Computer Science",
+      duration: "2 years",
+      description: "A comprehensive program in computer science.",
+      studentsCount: 50,
+      timings: "9:00 AM - 11:00 AM",
+    },
+  ];
+  const params = useParams();
+
+  const handleViewActivityClick = () => {
+    // Handle the click event for "View Activity" button here
+    console.log("View Activity button clicked");
+  };
+
   return (
     <>
-    <UserAppBar id={params.userId}/>
-    <div>RegisteredCourses</div>
+      <UserAppBar id={params.userId} />
+      <Card variant="outlined">
+        <CardContent>
+          <Typography variant="h6" component="div">
+            Course Name: {registeredCourse[0].courseName}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Duration: {registeredCourse[0].duration}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Description: {registeredCourse[0].description}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Students: {registeredCourse[0].studentsCount}
+          </Typography>
+          <Typography variant="body2" color="text.secondary">
+            Timings: {registeredCourse[0].timings}
+          </Typography>
+          <Button variant="contained" color="primary" onClick={handleViewActivityClick}>
+            View Activity
+          </Button>
+        </CardContent>
+      </Card>
     </>
-  )
-}
+  );
+};
 
-export default RegisteredCourses
+export default RegisteredCourses;
