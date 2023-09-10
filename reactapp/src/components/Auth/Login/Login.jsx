2fc7c35b1bfff21c88ddac8d36f53ba2336a85ff
Iamneo <techadmin@iamneo.ai>
2023-09-10 17:06:48 +0000

    Backup Commit

--- reactapp/src/components/Auth/Login/Login.jsx
+++ reactapp/src/components/Auth/Login/Login.jsx
@@ -11,14 +11,14 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
-import { Link , useNavigate} from 'react-router-dom';
-import BackgroundWrapper from '../../BackgroundWrapper'; 
+import { Link, useNavigate } from 'react-router-dom';
+import BackgroundWrapper from '../../BackgroundWrapper';
 import Footer from '../../Footer';
 import MenuItem from '@mui/material/MenuItem'; // Import MenuItem
 import Select from '@mui/material/Select'; // Import Select
 import axios from 'axios'; // Import axios for making API requests
-const defaultTheme = createTheme();
 
+const defaultTheme = createTheme();
 
 const Login = () => {
   const navigate = useNavigate();
@@ -27,6 +27,7 @@ const Login = () => {
   const [emailError, setEmailError] = useState(false);
   const [passwordError, setPasswordError] = useState(false);
   const [userType, setUserType] = useState('user');
+
   const validateEmail = (email) => {
     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(email);
@@ -44,55 +45,42 @@ const Login = () => {
       setPasswordError(true);
       return;
     }
-       
+
     const formData = {
       email: email,
       password: password,
-   
     };
-    
-    if(userType==="admin"){
-      const apiEndpoint ='https://8080-aceabfccabfeebedecececdedcceaefeeadb.premiumproject.examly.io/admin/login' 
-      
-            
+
+    if (userType === "admin") {
+      const apiEndpoint = 'https://8080-aceabfccabfeebedecececdedcceaefeeadb.premiumproject.examly.io/admin/login';
+
       axios
-      .post(apiEndpoint, formData)
-      .then((response) => {
-        console.log(response.data); 
-        navigate(`/AdminHomePage/UniversityList/${userId}`)
-      
-      })
-      .catch((error) => {
-        console.error(error); // Handle error
-      });
-
+        .post(apiEndpoint, formData)
+        .then((response) => {
+          console.log(response.data);
+          // Set the userId based on the response
+          const userId = response.data.userId;
+          navigate(`/AdminHomePage/UniversityList/${userId}`);
+        })
+        .catch((error) => {
+          console.error(error); // Handle error
+        });
     }
-    if(userType === "user"){
-      const apiEndpoint =  'https://8080-aceabfccabfeebedecececdedcceaefeeadb.premiumproject.examly.io/user/login'; 
+    if (userType === "user") {
+      const apiEndpoint = 'https://8080-aceabfccabfeebedecececdedcceaefeeadb.premiumproject.examly.io/user/login';
+
       axios
-      .post(apiEndpoint, formData)
-      .then((response) => {
-        console.log(response.data); 
-        navigate(`/HomePage/UniversityList/${userId}`);
-      
-      })
-      .catch((error) => {
-        console.error(error); // Handle error
-      });
+        .post(apiEndpoint, formData)
+        .then((response) => {
+          console.log(response.data);
+          // Set the userId based on the response
+          const userId = response.data.userId;
+          navigate(`/HomePage/UniversityList/${userId}`);
+        })
+        .catch((error) => {
+          console.error(error); // Handle error
+        });
     }
-
-   
-
-
-  
-    
-
-    
- 
-   
-   
-
-   
   };
 
   const handleEmailChange = (e) => {
@@ -105,35 +93,35 @@ const Login = () => {
     setPasswordError(false); // Clear password error when user starts typing
   };
 
+  const handleUserTypeChange = (e) => {
+    setUserType(e.target.value);
+  };
+
   return (
     <BackgroundWrapper>
-    
-    <ThemeProvider theme={defaultTheme}>
-      
-    <Container  component="main" maxWidth="xs">
-
-  
-        <CssBaseline />
-              <Box
-        sx={{
-          marginTop: 8,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          border: '1px solid #ccc',
-          borderRadius: '8px',
-          padding: '20px', // Add padding here
-          boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
-        }}
-      >
-          <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in to AdmitEasy
-          </Typography>
-          <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1, p : 3 }}>
-          <Select
+      <ThemeProvider theme={defaultTheme}>
+        <Container component="main" maxWidth="xs">
+          <CssBaseline />
+          <Box
+            sx={{
+              marginTop: 8,
+              display: 'flex',
+              flexDirection: 'column',
+              alignItems: 'center',
+              border: '1px solid #ccc',
+              borderRadius: '8px',
+              padding: '20px', // Add padding here
+              boxShadow: '0px 4px 8px rgba(0, 0, 0, 0.1)',
+            }}
+          >
+            <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+              <LockOutlinedIcon />
+            </Avatar>
+            <Typography component="h1" variant="h5">
+              Sign in to AdmitEasy
+            </Typography>
+            <Box component="form" onSubmit={handleSubmit} noValidate sx={{ mt: 1, p: 3 }}>
+              <Select
                 required
                 label="User Type"
                 id="userType"
@@ -143,69 +131,60 @@ const Login = () => {
                 <MenuItem value="user">User</MenuItem>
                 <MenuItem value="admin">Admin</MenuItem>
               </Select>
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-              value={email}
-              onChange={handleEmailChange}
-              error={emailError}
-              helperText={emailError ? 'Please enter a valid email address' : ''}
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-              value={password}
-              onChange={handlePasswordChange}
-              error={passwordError}
-              helperText={passwordError ? 'Password cannot be empty' : ''}
-            />
-
-
-
-
-
-
-
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              id="loginButton"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Login
-            </Button>
-            <Grid container>
-            
-              <Grid item>
-                <Link to="/Signup" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                id="email"
+                label="Email Address"
+                name="email"
+                autoComplete="email"
+                autoFocus
+                value={email}
+                onChange={handleEmailChange}
+                error={emailError}
+                helperText={emailError ? 'Please enter a valid email address' : ''}
+              />
+              <TextField
+                margin="normal"
+                required
+                fullWidth
+                name="password"
+                label="Password"
+                type="password"
+                id="password"
+                autoComplete="current-password"
+                value={password}
+                onChange={handlePasswordChange}
+                error={passwordError}
+                helperText={passwordError ? 'Password cannot be empty' : ''}
+              />
+              <FormControlLabel
+                control={<Checkbox value="remember" color="primary" />}
+                label="Remember me"
+              />
+              <Button
+                type="submit"
+                fullWidth
+                variant="contained"
+                id="loginButton"
+                sx={{ mt: 3, mb: 2 }}
+              >
+                Login
+              </Button>
+              <Grid container>
+                <Grid item>
+                  <Link to="/Signup" variant="body2">
+                    {"Don't have an account? Sign Up"}
+                  </Link>
+                </Grid>
               </Grid>
-            </Grid>
+            </Box>
           </Box>
-        </Box>
-      </Container>
-   <Footer/>
-    </ThemeProvider>
+        </Container>
+        <Footer />
+      </ThemeProvider>
     </BackgroundWrapper>
-   
   );
 };
 
