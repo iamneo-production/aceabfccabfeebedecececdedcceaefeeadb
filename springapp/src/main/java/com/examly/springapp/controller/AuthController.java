de2c2d07d3bdc0ab1c5ba89fc7bf728abb497ef8
Iamneo <techadmin@iamneo.ai>
2023-09-10 15:54:02 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/AuthController.java
+++ springapp/src/main/java/com/examly/springapp/controller/AuthController.java
@@ -1,3 +1,30 @@
+// package com.examly.springapp.controller;
+
+// import com.examly.springapp.model.User;
+// import com.examly.springapp.service.UserService;
+// import org.springframework.beans.factory.annotation.Autowired;
+// import org.springframework.web.bind.annotation.*;
+
+// import java.util.List;
+// @CrossOrigin
+// @RestController
+// @RequestMapping("/user")
+
+// public class AuthController {
+//     @Autowired
+//     private UserService userService;
+
+//     @PostMapping("/add")
+//     public String add(@RequestBody User user){
+//         userService.saveUser(user);
+//         return "New User has been added";
+//     }
+//     @GetMapping("/getAll")
+//     public List<User> getAllUsers(){
+//         return userService.getAllUsers();
+//     }
+
+// }
 package com.examly.springapp.controller;
 
 import com.examly.springapp.model.User;
@@ -6,22 +33,42 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
+
 @CrossOrigin
 @RestController
-@RequestMapping("/user")
 
 public class AuthController {
     @Autowired
     private UserService userService;
 
-    @PostMapping("/add")
-    public String add(@RequestBody User user){
+    @PostMapping("/user/signup")
+    public String signup(@RequestBody User user) {
         userService.saveUser(user);
         return "New User has been added";
     }
-    @GetMapping("/getAll")
-    public List<User> getAllUsers(){
-        return userService.getAllUsers();
+
+    @PostMapping("/user/login")
+    public Long userLogin(@RequestBody User loginUser) {
+        Optional<User> user = userService.getUserByEmailAndPassword(loginUser.getEmail(), loginUser.getPassword());
+        return user.map(User::getUserId).orElse(null);
     }
 
+    @PostMapping("/admin/signup")
+    public String adminSignup(@RequestBody User adminUser) {
+        // Implement admin signup logic here
+        userService.saveUser(adminUser);
+        return "New Admin User has been added";
+    }
+
+    @PostMapping("/admin/login")
+    public Long adminLogin(@RequestBody User adminLoginUser) {
+        Optional<User> adminUser = userService.getAdminByEmailAndPassword(adminLoginUser.getEmail(), adminLoginUser.getPassword());
+        return adminUser.map(User::getUserId).orElse(null);
+    }
+
+    @GetMapping("/user/getAll")
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
 }
