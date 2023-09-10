24df50a8dbf0c91812c4a595250a93b4d01d5f12
Iamneo <techadmin@iamneo.ai>
2023-09-10 05:31:41 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/UserController.java
+++ springapp/src/main/java/com/examly/springapp/controller/UserController.java
@@ -6,9 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin
 @RestController
 @RequestMapping("/user")
+
 public class UserController {
     @Autowired
     private UserService userService;
