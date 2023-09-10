c3270580dd8b11251b64c0cfe9b8275bec3e8fea
Iamneo <techadmin@iamneo.ai>
2023-09-10 15:10:28 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/AuthController.java
+++ springapp/src/main/java/com/examly/springapp/controller/AuthController.java
@@ -10,7 +10,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 
-public class UserController {
+public class AuthController {
     @Autowired
     private UserService userService;
 
