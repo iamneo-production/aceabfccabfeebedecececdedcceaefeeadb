54249f9319ad254fcb1d53dee6e2072b6597949b
Iamneo <techadmin@iamneo.ai>
2023-09-10 17:31:48 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/AuthController.java
+++ springapp/src/main/java/com/examly/springapp/controller/AuthController.java
@@ -31,9 +31,11 @@ import com.examly.springapp.model.User;
 import com.examly.springapp.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import java.util.*;
 
-import java.util.List;
-import java.util.Optional;
+import org.springframework.http.HttpStatus;
+
 
 @CrossOrigin
 @RestController
@@ -53,19 +55,44 @@ public class AuthController {
         userService.saveUser(adminUser);
         return "New Admin User has been added";
     }
+    // @PostMapping("/user/login")
+    // public Long userLogin(@RequestBody User loginUser) {
+    //     Optional<User> user = userService.getUserByEmailAndPassword(loginUser.getEmail(), loginUser.getPassword());
+    //     return user.map(User::getUserId).orElse(null);
+    // }
     @PostMapping("/user/login")
-    public Long userLogin(@RequestBody User loginUser) {
-        Optional<User> user = userService.getUserByEmailAndPassword(loginUser.getEmail(), loginUser.getPassword());
-        return user.map(User::getUserId).orElse(null);
-    }
+public ResponseEntity<Map<String, Long>> userLogin(@RequestBody User loginUser) {
+Optional<User> user = userService.getUserByEmailAndPassword(loginUser.getEmail(), loginUser.getPassword());
+
+Map<String, Long> response = new HashMap<>();
+if (user.isPresent()) {
+    response.put("userId", user.get().getUserId());
+    return ResponseEntity.ok(response);
+} else {
+    return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+}
+}
 
    
 
+    // @PostMapping("/admin/login")
+    // public Long adminLogin(@RequestBody User adminLoginUser) {
+    //     Optional<User> adminUser = userService.getAdminByEmailAndPassword(adminLoginUser.getEmail(), adminLoginUser.getPassword());
+    //     return adminUser.map(User::getUserId).orElse(null);
+    // }
     @PostMapping("/admin/login")
-    public Long adminLogin(@RequestBody User adminLoginUser) {
-        Optional<User> adminUser = userService.getAdminByEmailAndPassword(adminLoginUser.getEmail(), adminLoginUser.getPassword());
-        return adminUser.map(User::getUserId).orElse(null);
+public ResponseEntity<Map<String, Long>> adminLogin(@RequestBody User adminLoginUser) {
+    Optional<User> adminUser = userService.getAdminByEmailAndPassword(adminLoginUser.getEmail(), adminLoginUser.getPassword());
+
+    Map<String, Long> response = new HashMap<>();
+    if (adminUser.isPresent()) {
+        response.put("userId", adminUser.get().getUserId());
+        return ResponseEntity.ok(response);
+    } else {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
+}
+
 
     @GetMapping("/user/getAll")
     public List<User> getAllUsers() {
