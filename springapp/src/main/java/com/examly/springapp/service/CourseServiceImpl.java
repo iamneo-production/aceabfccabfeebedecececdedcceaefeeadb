add4cac19e495bcaaf7ea0b543332324b7c29d69
Iamneo <techadmin@iamneo.ai>
2023-09-12 06:09:04 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/service/CourseServiceImpl.java
+++ springapp/src/main/java/com/examly/springapp/service/CourseServiceImpl.java
@@ -1,3 +1,46 @@
-public class CourseServiceImpl {
-    
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CourseServiceImpl implements CourseService {
+    @Autowired
+    private CourseRepository courseRepository;
+
+    @Override
+    public CourseModel createCourse(CourseModel course) {
+        return courseRepository.save(course);
+    }
+
+    @Override
+    public Optional<CourseModel> getCourseById(int courseId) {
+        return courseRepository.findById(courseId);
+    }
+
+    @Override
+    public List<CourseModel> getAllCourses() {
+        return courseRepository.findAll();
+    }
+
+    @Override
+    public CourseModel updateCourse(int courseId, CourseModel updatedCourse) {
+        Optional<CourseModel> existingCourse = courseRepository.findById(courseId);
+
+        if (existingCourse.isPresent()) {
+            updatedCourse.setCourseId(courseId);
+            return courseRepository.save(updatedCourse);
+        } else {
+            // Handle the case when the course with the given ID does not exist.
+            return null; // or throw an exception
+        }
+    }
+
+    @Override
+    public void deleteCourse(int courseId) {
+        courseRepository.deleteById(courseId);
+    }
+
+    // Implement custom service methods if needed
 }
