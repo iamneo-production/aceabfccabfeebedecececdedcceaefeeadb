7d7d84bbb469c94f391f35d05c520a5245544402
Iamneo <techadmin@iamneo.ai>
2023-09-13 15:40:02 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/AuthController.java
+++ springapp/src/main/java/com/examly/springapp/controller/AuthController.java
@@ -18,38 +18,38 @@ public class AuthController {
     @Autowired
     private UserService userService;
 
-   
+
     @PostMapping("/user/signup")
-public ResponseEntity<String> signup(@RequestBody UserModel user) {
-    // Check if the email already exists in the user table
-    if (userService.getUserByEmail(user.getEmail()).isPresent()) {
-        return ResponseEntity.ok("Email already exists");
+    public ResponseEntity<String> signup(@RequestBody UserModel user) {
+        // Check if the email already exists in the user table
+        if (userService.getUserByEmail(user.getEmail()).isPresent()) {
+            return ResponseEntity.ok("Email already exists");
+        }
+        // If the email doesn't exist, proceed with user registration
+        else{
+            userService.saveUser(user);
+            return ResponseEntity.ok("New User has been added");
+        }
     }
-    // If the email doesn't exist, proceed with user registration
-    else{
-    userService.saveUser(user);
-    return ResponseEntity.ok("New User has been added");
-    }
-}
 
 
     @PostMapping("/admin/signup")
-public ResponseEntity<String> adminSignup(@RequestBody UserModel adminUser) {
-    // Check if the email already exists in the user table
-    if (userService.getUserByEmail(adminUser.getEmail()).isPresent()) {
-        return ResponseEntity.ok("Email already exists");
-    }
+    public ResponseEntity<String> adminSignup(@RequestBody UserModel adminUser) {
+        // Check if the email already exists in the user table
+        if (userService.getUserByEmail(adminUser.getEmail()).isPresent()) {
+            return ResponseEntity.ok("Email already exists");
+        }
 
-    // If the email doesn't exist, proceed with admin user registration
-    userService.saveUser(adminUser);
-    return ResponseEntity.ok("New Admin User has been added");
-}
+        // If the email doesn't exist, proceed with admin user registration
+        userService.saveUser(adminUser);
+        return ResponseEntity.ok("New Admin User has been added");
+    }
 
 
     @PostMapping("/user/login")
     public ResponseEntity<Map<String, String>> userLogin(@RequestBody UserModel loginUser) {
         Optional<UserModel> user = userService.getUserByEmailAndPassword(loginUser.getEmail(), loginUser.getPassword());
-    
+
         Map<String, String> response = new HashMap<>();
         if (user.isPresent()) {
             response.put("userId", user.get().getUserId().toString());
@@ -59,24 +59,24 @@ public ResponseEntity<String> adminSignup(@RequestBody UserModel adminUser) {
             return ResponseEntity.ok(response);
         }
     }
-    
 
 
 
 
-@PostMapping("/admin/login")
-public ResponseEntity<Map<String, String>> adminLogin(@RequestBody UserModel adminLoginUser) {
-    Optional<UserModel> adminUser = userService.getAdminByEmailAndPassword(adminLoginUser.getEmail(), adminLoginUser.getPassword());
 
-    Map<String, String> response = new HashMap<>();
-    if (adminUser.isPresent()) {
-        response.put("userId", adminUser.get().getUserId().toString());
-        return ResponseEntity.ok(response);
-    } else {
-        response.put("userId", "Not Found");
-        return ResponseEntity.ok(response);
+    @PostMapping("/admin/login")
+    public ResponseEntity<Map<String, String>> adminLogin(@RequestBody UserModel adminLoginUser) {
+        Optional<UserModel> adminUser = userService.getAdminByEmailAndPassword(adminLoginUser.getEmail(), adminLoginUser.getPassword());
+
+        Map<String, String> response = new HashMap<>();
+        if (adminUser.isPresent()) {
+            response.put("userId", adminUser.get().getUserId().toString());
+            return ResponseEntity.ok(response);
+        } else {
+            response.put("userId", "Not Found");
+            return ResponseEntity.ok(response);
+        }
     }
-}
 
 
 
