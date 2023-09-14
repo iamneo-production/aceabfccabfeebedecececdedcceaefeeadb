1346db16d3e4c2f315e530cb7e5fca8c34e9144d
Iamneo <techadmin@iamneo.ai>
2023-09-14 03:30:53 +0000

    Backup Commit

--- reactapp/src/components/User/ApplyForm/ApplyForm.jsx
+++ reactapp/src/components/User/ApplyForm/ApplyForm.jsx
@@ -1,4 +1,4 @@
-import React, { useState , useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -11,9 +11,9 @@ import Footer from "../../Footer";
 import axios from "axios";
 
 const ApplyForm = ({ collegeId, title, onClose }) => {
-  console.log(collegeId)
+  console.log(collegeId);
 
-  const [courseList,setCourseList] = useState([]);
+  const [courseList, setCourseList] = useState([]);
   const [filteredCourses, setFilteredCourses] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedCourse, setSelectedCourse] = useState(null);
@@ -21,24 +21,17 @@ const ApplyForm = ({ collegeId, title, onClose }) => {
 
   useEffect(() => {
     axios
-      .get(`https://8080-aceabfccabfeebedecececdedcceaefeeadb.premiumproject.examly.io/admin/coursesByInstitute/${collegeId}`)
+      .get(
+        `https://8080-aceabfccabfeebedecececdedcceaefeeadb.premiumproject.examly.io/admin/coursesByInstitute/${collegeId}`
+      )
       .then((response) => {
-        // Use a callback function to set the courseList state with the response data
         setCourseList(response.data);
-        setFilteredCourses(response.data); // Set filteredCourses with the response data
-        
+        setFilteredCourses(response.data);
       })
       .catch((error) => {
         console.error("Error fetching course data:", error);
       });
   }, [collegeId]);
-  
-  
-
-
-
-
-
 
   const [formData, setFormData] = useState({
     firstName: "",
@@ -62,7 +55,7 @@ const ApplyForm = ({ collegeId, title, onClose }) => {
 
   const handleEnrollClick = (course) => {
     setSelectedCourse(course);
-    setFormData({}); 
+    setFormData({});
     setEnrollFormOpen(true);
   };
 
@@ -71,22 +64,17 @@ const ApplyForm = ({ collegeId, title, onClose }) => {
   };
 
   const handleSearchChange = (e) => {
-    setSearchQuery(e.target.value);
-  };
-
-  const handleSearch = () => {
+    const query = e.target.value.toLowerCase();
     const filtered = courseList.filter((course) =>
-      course.courseName.toLowerCase().includes(searchQuery.toLowerCase())
+      course.courseName.toLowerCase().includes(query)
     );
     setFilteredCourses(filtered);
+    setSearchQuery(query);
   };
 
   const handleFormSubmit = (e) => {
     e.preventDefault();
-    // Log form details
     console.log("Form Data:", formData);
-
-    // Add more fields as needed
     handleCloseEnrollForm();
   };
 
@@ -112,107 +100,60 @@ const ApplyForm = ({ collegeId, title, onClose }) => {
       </Grid>
 
       <div style={{ display: "flex", alignItems: "center", margin: "10px 0" }}>
-  <TextField
-    label="Search by Course Title"
-    variant="outlined"
-    value={searchQuery}
-    onChange={handleSearchChange}
-    fullWidth
-    style={{ flex: 1, marginRight: "10px" }}
-  />
-  <Button
-    variant="contained"
-    color="primary"
-    onClick={handleSearch}
-    style={{ minWidth: "auto" }}
-  >
-    Search
-  </Button>
-</div>
+        <TextField
+          label="Search by Course Title"
+          variant="outlined"
+          value={searchQuery}
+          onChange={handleSearchChange}
+          fullWidth
+          style={{ flex: 1, marginRight: "10px" }}
+        />
+      </div>
 
-
-      
-
-
-{/* <Grid container spacing={2}>
-  {filteredCourses.map((course) => (
-    <Grid key={course.courseId} item xs={12}>
-      <Card style={{ width: '100%' }}>
-        <CardContent style={{ textAlign: "left" }}>
-          <Typography variant="h6" component="div">
-            Course Name: {course.courseName}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Duration: {course.duration}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Description: {course.description}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Students: {course.studentsCount}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Timings: {course.timings}
-          </Typography>
-        </CardContent>
-        <div style={{ display: 'flex', justifyContent: 'flex-end', padding: '8px' }}>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => handleEnrollClick(course)}
-          >
-            Enroll Now
-          </Button>
-        </div>
-      </Card>
-    </Grid>
-  ))}
-</Grid>
- */}
-
-<Grid container spacing={2}>
-  {courseList.map((course) => (
-    <Grid key={course.courseId} item xs={12}>
-      <Card style={{ width: '100%' }}>
-        <CardContent style={{ textAlign: "left" }}>
-          <Typography variant="h6" component="div">
-            Course Name: {course.courseName}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Duration: {course.duration}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Description: {course.description}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Students: {course.studentsCount}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Timings: {course.timings}
-          </Typography>
-        </CardContent>
-        <div style={{ display: 'flex', justifyContent: 'flex-end', padding: '8px' }}>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => handleEnrollClick(course)}
-          >
-            Enroll Now
-          </Button>
-        </div>
-      </Card>
-    </Grid>
-  ))}
-</Grid>
-
-
-
-
-      
+      <Grid container spacing={2}>
+        {filteredCourses.map((course) => (
+          <Grid key={course.courseId} item xs={12}>
+            <Card style={{ width: "100%" }}>
+              <CardContent style={{ textAlign: "left" }}>
+                <Typography variant="h6" component="div">
+                  Course Name: {course.course_name} {/* Adjust the property name */}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Duration: {course.duration} {/* Adjust the property name */}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Description: {course.description} {/* Adjust the property name */}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Students: {course.students_count} {/* Adjust the property name */}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Timings: {course.timings} {/* Adjust the property name */}
+                </Typography>
+              </CardContent>
+              <div
+                style={{
+                  display: "flex",
+                  justifyContent: "flex-end",
+                  padding: "8px",
+                }}
+              >
+                <Button
+                  variant="contained"
+                  color="primary"
+                  onClick={() => handleEnrollClick(course)}
+                >
+                  Enroll Now
+                </Button>
+              </div>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
 
       <Dialog open={enrollFormOpen} onClose={handleCloseEnrollForm}>
         <DialogContent>
-          <h3>Enrollment Form for {selectedCourse?.courseName}</h3>
+          <h3>Enrollment Form for {selectedCourse?.course_name}</h3>
           <form onSubmit={handleFormSubmit}>
             <Grid container spacing={2}>
               <Grid item xs={4}>
@@ -226,177 +167,7 @@ const ApplyForm = ({ collegeId, title, onClose }) => {
                   onChange={handleInputChange}
                 />
               </Grid>
-              <Grid item xs={4}>
-                <TextField
-                  label="Last Name"
-                  fullWidth
-                  id="lastName"
-                  margin="normal"
-                  variant="outlined"
-                  value={formData.lastName}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <TextField
-                  label="Gender"
-                  fullWidth
-                  id="gender"
-                  margin="normal"
-                  variant="outlined"
-                  value={formData.gender}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <TextField
-                  label="Father's Name"
-                  fullWidth
-                  id="fatherName"
-                  margin="normal"
-                  variant="outlined"
-                  value={formData.fatherName}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <TextField
-                  label="Phone Number 1"
-                  fullWidth
-                  id="phoneNumber1"
-                  margin="normal"
-                  variant="outlined"
-                  value={formData.phoneNumber1}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <TextField
-                  label="Phone Number 2"
-                  fullWidth
-                  id="phoneNumber2"
-                  margin="normal"
-                  variant="outlined"
-                  value={formData.phoneNumber2}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-            </Grid>
-            <Grid container spacing={2}>
-              <Grid item xs={4}>
-                <TextField
-                  label="Mother's Name"
-                  fullWidth
-                  id="motherName"
-                  margin="normal"
-                  variant="outlined"
-                  value={formData.motherName}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-            </Grid>
-            <Grid container spacing={2}>
-              <Grid item xs={4}>
-                <TextField
-                  label="House No"
-                  fullWidth
-                  id="houseNo"
-                  margin="normal"
-                  variant="outlined"
-                  value={formData.houseNo}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <TextField
-                  label="Street Name"
-                  fullWidth
-                  id="streetName"
-                  margin="normal"
-                  variant="outlined"
-                  value={formData.streetName}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <TextField
-                  label="Area Name"
-                  fullWidth
-                  id="areaName"
-                  margin="normal"
-                  variant="outlined"
-                  value={formData.areaName}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <TextField
-                  label="Pin Code"
-                  fullWidth
-                  id="pinCode"
-                  margin="normal"
-                  variant="outlined"
-                  value={formData.pinCode}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <TextField
-                  label="State"
-                  fullWidth
-                  id="state"
-                  margin="normal"
-                  variant="outlined"
-                  value={formData.state}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <TextField
-                  label="Nationality"
-                  fullWidth
-                  id="nationality"
-                  margin="normal"
-                  variant="outlined"
-                  value={formData.nationality}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-            </Grid>
-            <Grid container spacing={2}>
-              <Grid item xs={4}>
-                <TextField
-                  label="Email"
-                  fullWidth
-                  id="email"
-                  margin="normal"
-                  variant="outlined"
-                  value={formData.email}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <TextField
-                  label="Age"
-                  fullWidth
-                  id="age"
-                  margin="normal"
-                  variant="outlined"
-                  value={formData.age}
-                  onChange={handleInputChange}
-                />
-              </Grid>
-              <Grid item xs={4}>
-                <TextField
-                  label="SSLC/HSC Marks"
-                  fullWidth
-                  id="sslcMarks"
-                  margin="normal"
-                  variant="outlined"
-                  value={formData.sslcMarks}
-                  onChange={handleInputChange}
-                />
-              </Grid>
+              {/* Add more form fields here */}
             </Grid>
             <Button variant="contained" color="primary" type="submit">
               Save
@@ -404,7 +175,7 @@ const ApplyForm = ({ collegeId, title, onClose }) => {
           </form>
         </DialogContent>
       </Dialog>
-      <Footer/>
+      <Footer />
     </div>
   );
 };
