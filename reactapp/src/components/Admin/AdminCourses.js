8a399d484420ffba3f4156b8c4079c6e14bd6dc2
Iamneo <techadmin@iamneo.ai>
2023-09-14 03:51:14 +0000

    Backup Commit

--- reactapp/src/components/Admin/AdminCourses.js
+++ reactapp/src/components/Admin/AdminCourses.js
@@ -1,6 +1,4 @@
-
-
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
@@ -9,93 +7,11 @@ import Dialog from "@mui/material/Dialog";
 import DialogContent from "@mui/material/DialogContent";
 import TextField from "@mui/material/TextField";
 import Grid from "@mui/material/Grid";
+import axios from "axios";
 
 const AdminCourses = ({ collegeId, title, onClose }) => {
-  const courseList = [
-    {
-      courseId: 1,
-      courseName: "Masters in Computer Science",
-      duration: "2 years",
-      description: "A comprehensive program in computer science.",
-      studentsCount: 50,
-      timings: "9:00 AM - 11:00 AM",
-    },
-    {
-      courseId: 2,
-      courseName: "Masters in Business Administration (MBA)",
-      duration: "2 years",
-      description: "Advanced studies in business management.",
-      studentsCount: 40,
-      timings: "10:00 AM - 12:00 PM",
-    },
-    {
-      courseId: 3,
-      courseName: "Masters in Electrical Engineering",
-      duration: "2 years",
-      description: "In-depth knowledge of electrical systems.",
-      studentsCount: 30,
-      timings: "11:00 AM - 1:00 PM",
-    },
-    {
-      courseId: 4,
-      courseName: "Masters in Data Science",
-      duration: "2 years",
-      description: "Harnessing the power of data for insights.",
-      studentsCount: 35,
-      timings: "2:00 PM - 4:00 PM",
-    },
-    {
-      courseId: 5,
-      courseName: "Masters in Psychology",
-      duration: "2 years",
-      description: "Understanding the human mind and behavior.",
-      studentsCount: 25,
-      timings: "3:00 PM - 5:00 PM",
-    },
-    {
-      courseId: 6,
-      courseName: "Masters in Civil Engineering",
-      duration: "2 years",
-      description: "Design and construction of infrastructure.",
-      studentsCount: 28,
-      timings: "9:30 AM - 11:30 AM",
-    },
-    {
-      courseId: 7,
-      courseName: "Masters in Environmental Science",
-      duration: "2 years",
-      description: "Exploring environmental issues and solutions.",
-      studentsCount: 20,
-      timings: "10:30 AM - 12:30 PM",
-    },
-    {
-      courseId: 8,
-      courseName: "Masters in Public Health",
-      duration: "2 years",
-      description: "Promoting health in communities.",
-      studentsCount: 22,
-      timings: "1:30 PM - 3:30 PM",
-    },
-    {
-      courseId: 9,
-      courseName: "Masters in Finance",
-      duration: "2 years",
-      description: "Managing financial resources effectively.",
-      studentsCount: 18,
-      timings: "2:30 PM - 4:30 PM",
-    },
-    {
-      courseId: 10,
-      courseName: "Masters in Linguistics",
-      duration: "2 years",
-      description: "Studying language and communication.",
-      studentsCount: 15,
-      timings: "3:30 PM - 5:30 PM",
-    },
-    // Add more courses as needed
-  ];
-
-  const [filteredCourses, setFilteredCourses] = useState(courseList);
+  const [courseList, setCourseList] = useState([]);
+  const [filteredCourses, setFilteredCourses] = useState([]);
   const [searchQuery, setSearchQuery] = useState("");
   const [selectedCourse, setSelectedCourse] = useState(null);
   const [editFormOpen, setEditFormOpen] = useState(false);
@@ -119,6 +35,19 @@ const AdminCourses = ({ collegeId, title, onClose }) => {
     timings: "",
   });
 
+  useEffect(() => {
+    // Fetch the course list from your API
+    axios
+      .get(`https://8080-aceabfccabfeebedecececdedcceaefeeadb.premiumproject.examly.io/admin/coursesByInstitute/${collegeId}`)
+      .then((response) => {
+        setCourseList(response.data);
+        setFilteredCourses(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching course data:", error);
+      });
+  }, [collegeId]);
+
   const handleEnrollClick = (course) => {
     setSelectedCourse(course);
     setEditFormOpen(true);
@@ -129,20 +58,22 @@ const AdminCourses = ({ collegeId, title, onClose }) => {
   };
 
   const handleSearchChange = (e) => {
-    setSearchQuery(e.target.value);
+    const { value } = e.target;
+    setSearchQuery(value);
+    // Call the handleSearch function immediately on input change
+    handleSearch(value);
   };
 
-  const handleSearch = () => {
+  const handleSearch = (query) => {
     const filtered = courseList.filter((course) => {
-      const nameMatch = course.courseName.toLowerCase().includes(searchQuery.toLowerCase());
-      const durationMatch = course.duration.toLowerCase().includes(searchQuery.toLowerCase());
-      const timingsMatch = course.timings.toLowerCase().includes(searchQuery.toLowerCase());
-      const descriptionMatch = course.description.toLowerCase().includes(searchQuery.toLowerCase())
+      const nameMatch = course.courseName.toLowerCase().includes(query.toLowerCase());
+      const durationMatch = course.duration.toLowerCase().includes(query.toLowerCase());
+      const timingsMatch = course.timings.toLowerCase().includes(query.toLowerCase());
+      const descriptionMatch = course.description.toLowerCase().includes(query.toLowerCase());
       return nameMatch || durationMatch || timingsMatch || descriptionMatch;
     });
     setFilteredCourses(filtered);
   };
-  
 
   const handleEditClick = (course) => {
     setSelectedCourse(course);
@@ -229,93 +160,60 @@ const AdminCourses = ({ collegeId, title, onClose }) => {
         fullWidth
         style={{ margin: "10px 0" }}
       />
-      <Button variant="contained" color="primary" onClick={handleSearch}>
-        Search
-      </Button>
 
- 
-      {/* <Grid container spacing={2}>
-  {filteredCourses.map((course) => (
-    <Grid key={course.courseId} item xs={12}>
-      <Card>
-        <CardContent>
-          <Typography variant="h6" component="div">
-            Course Name: {course.courseName}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Duration: {course.duration}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Description: {course.description}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Students: {course.studentsCount}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Timings: {course.timings}
-          </Typography>
-          <Button
-            variant="contained"
-            color="primary"
-            onClick={() => handleEditClick(course)}
-          >
-            Edit
-          </Button>
-          <Button
-            variant="contained"
-            color="secondary"
-            onClick={() => handleDeleteClick(course.courseId)}
-          >
-            Delete
-          </Button>
-        </CardContent>
-      </Card>
-    </Grid>
-  ))}
-</Grid> */}
-
-<Grid container spacing={2}>
-  {filteredCourses.map((course) => (
-    <Grid key={course.courseId} item xs={12}>
-      <Card>
-        <CardContent style={{ display: 'flex', flexDirection: 'column', alignItems: 'flex-start' }}>
-          <Typography variant="h6" component="div">
-            Course Name: {course.courseName}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Duration: {course.duration}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Description: {course.description}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Students: {course.studentsCount}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Timings: {course.timings}
-          </Typography>
-          <div style={{ marginTop: 'auto', display: 'flex', justifyContent: 'space-between' }}>
-            <Button
-              variant="contained"
-              color="primary"
-              onClick={() => handleEditClick(course)}
-            >
-              Edit
-            </Button>
-            <Button
-              variant="contained"
-              color="secondary"
-              onClick={() => handleDeleteClick(course.courseId)}
-            >
-              Delete
-            </Button>
-          </div>
-        </CardContent>
-      </Card>
-    </Grid>
-  ))}
-</Grid>
-
+      <Grid container spacing={2}>
+        {filteredCourses.map((course) => (
+          <Grid key={course.courseId} item xs={12}>
+            <Card>
+              <CardContent
+                style={{
+                  display: "flex",
+                  flexDirection: "column",
+                  alignItems: "flex-start",
+                }}
+              >
+                <Typography variant="h6" component="div">
+                  Course Name: {course.courseName}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Duration: {course.duration}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Description: {course.description}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Students: {course.studentsCount}
+                </Typography>
+                <Typography variant="body2" color="text.secondary">
+                  Timings: {course.timings}
+                </Typography>
+                <div
+                  style={{
+                    marginTop: "auto",
+                    display: "flex",
+                    justifyContent: "space-between",
+                  }}
+                >
+                  <Button
+                    variant="contained"
+                    color="primary"
+                    onClick={() => handleEditClick(course)}
+                  >
+                    Edit
+                  </Button>
+                  <Button
+                    variant="contained"
+                    color="secondary"
+                    onClick={() => handleDeleteClick(course.courseId)}
+                  >
+                    Delete
+                  </Button>
+                </div>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
 
       <Dialog open={editFormOpen} onClose={handleCloseEditForm}>
         <DialogContent>
