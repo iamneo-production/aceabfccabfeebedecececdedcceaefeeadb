12d43d0598e22983b79f8e4e075bc139c114ac98
Iamneo <techadmin@iamneo.ai>
2023-09-15 09:58:44 +0000

    Backup Commit

--- reactapp/src/components/User/HomePage/RegisteredCourses.jsx
+++ reactapp/src/components/User/HomePage/RegisteredCourses.jsx
@@ -4,28 +4,8 @@ import UserAppBar from '../../UserAppBar';
 import { Card, CardContent, Typography, Button } from '@mui/material';
 import Footer from '../../Footer';
 
-const containerStyle = {
-  display: 'flex',
-  flexDirection: 'column',
-  minHeight: '100vh',
-};
-
-const contentStyle = {
-  flex: '1', /* This makes the content area expand to fill available space */
-  textAlign: 'left', /* Align the card content to the left */
-};
-
 const RegisteredCourses = () => {
-  const registeredCourse = [
-    {
-      courseId: 1,
-      courseName: "Masters in Computer Science",
-      duration: "2 years",
-      description: "A comprehensive program in computer science.",
-      studentsCount: 50,
-      timings: "9:00 AM - 11:00 AM",
-    },
-  ];
+  const [registeredCourses, setRegisteredCourses] = useState([]);
   const params = useParams();
 
   const handleViewActivityClick = () => {
@@ -33,38 +13,56 @@ const RegisteredCourses = () => {
     console.log("View Activity button clicked");
   };
 
+  const handleDeleteClick = (courseId) => {
+    // Handle the click event for the delete button here
+    console.log("Delete button clicked for course ID:", courseId);
+  };
+
+  useEffect(() => {
+    // Fetch the registered courses list from the server
+    axios
+      .get(`https://8080-aceabfccabfeebedecececdedcceaefeeadb.premiumproject.examly.io/admin/viewAdmissionByUserId/${params.userId}`)
+      .then((response) => {
+        setRegisteredCourses(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching registered courses:", error);
+      });
+  }, [params.userId]);
+
   return (
     <div style={containerStyle}>
-      
       <UserAppBar id={params.userId} />
       <div style={contentStyle}>
-        
-        <Card variant="outlined">
-          <CardContent>
-            <Typography variant="h6" component="div">
-              Course Name: {registeredCourse[0].courseName}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Duration: {registeredCourse[0].duration}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Description: {registeredCourse[0].description}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Students: {registeredCourse[0].studentsCount}
-            </Typography>
-            <Typography variant="body2" color="text.secondary">
-              Timings: {registeredCourse[0].timings}
-            </Typography>
-            <Button variant="contained" color="primary" onClick={handleViewActivityClick}>
-              View Activity
-            </Button>
-          </CardContent>
-        </Card>
+        {registeredCourses.map((course) => (
+          <Card key={course.admissionId} variant="outlined">
+            <CardContent>
+              <Typography variant="h6" component="div">
+                Course Name: {course.courseName}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Duration: {course.duration}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Description: {course.description}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Students: {course.studentsCount}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Timings: {course.timings}
+              </Typography>
+              <Button variant="contained" color="primary" onClick={handleViewActivityClick}>
+                View Activity
+              </Button>
+              <Button variant="contained" color="secondary" onClick={() => handleDeleteClick(course.admissionId)}>
+                Delete
+              </Button>
+            </CardContent>
+          </Card>
+        ))}
       </div>
       <Footer />
     </div>
   );
 };
-
-export default RegisteredCourses;
