d4f7daccbd7071ffc6b50974170f92a11a49aa45
Iamneo <techadmin@iamneo.ai>
2023-09-15 02:29:03 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/model/AdmissionModel.java
+++ springapp/src/main/java/com/examly/springapp/model/AdmissionModel.java
@@ -11,7 +11,7 @@ public class AdmissionModel {
     private int courseId;
     private int instituteId;
     private String status;
-    
+
     @Column(name = "student_id") // New column for studentId
     private int studentId;
 
@@ -20,68 +20,7 @@ public class AdmissionModel {
 
     // Constructors...
 
-    // Default constructor
-    public AdmissionModel() {
-    }
-
-    // Parameterized constructor
-    public AdmissionModel(int courseId, int instituteId, String status, int studentId, int userId) {
-        this.courseId = courseId;
-        this.instituteId = instituteId;
-        this.status = status;
-        this.studentId = studentId;
-        this.userId = userId;
-    }
-
     // Getters and setters...
 
     // Update getters and setters for studentId and userId accordingly.
-
-    public int getAdmissionId() {
-        return admissionId;
-    }
-
-    public void setAdmissionId(int admissionId) {
-        this.admissionId = admissionId;
-    }
-
-    public int getCourseId() {
-        return courseId;
-    }
-
-    public void setCourseId(int courseId) {
-        this.courseId = courseId;
-    }
-
-    public int getInstituteId() {
-        return instituteId;
-    }
-
-    public void setInstituteId(int instituteId) {
-        this.instituteId = instituteId;
-    }
-
-    public String getStatus() {
-        return status;
-    }
-
-    public void setStatus(String status) {
-        this.status = status;
-    }
-
-    public int getStudentId() {
-        return studentId;
-    }
-
-    public void setStudentId(int studentId) {
-        this.studentId = studentId;
-    }
-
-    public int getUserId() {
-        return userId;
-    }
-
-    public void setUserId(int userId) {
-        this.userId = userId;
-    }
 }
