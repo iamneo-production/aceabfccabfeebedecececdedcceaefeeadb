0fbb39dc14ac2a80e8039c367a570bc96550ef23
Iamneo <techadmin@iamneo.ai>
2023-09-15 03:19:08 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/controller/AdmissionController.java
+++ springapp/src/main/java/com/examly/springapp/controller/AdmissionController.java
@@ -31,22 +31,36 @@ public class AdmissionController {
         AdmissionModel createdAdmission = admissionService.createAdmission(admission);
         return ResponseEntity.ok(createdAdmission);
     }
+
+
     @PostMapping("/addAdmissionNew/{userId}")
-    public ResponseEntity<AdmissionModel> addAdmission(
-            @RequestBody AdmissionModel admission,
-            @PathVariable int userId
-    ) {
-        // Fetch the corresponding studentId from the Student table using userId
-        int studentId = studentService.getStudentIdByUserId(userId);
-        AdmissionModel admission = new AdmissionModel(courseId, instituteId, "enrolled", studentId, userId);
-        admissionService.createAdmission(admission, courseId, userId);
+public ResponseEntity<AdmissionModel> addAdmission(
+        @PathVariable int userId,
+        @RequestParam int courseId,
+        @RequestParam int instituteId,
+        @RequestParam String status
+) {
+    // Fetch the corresponding studentId from the Student table using userId
+    int studentId = studentService.getStudentIdByUserId(userId);
+    
+    // Create a new AdmissionModel using the provided data
+    AdmissionModel admission = new AdmissionModel(courseId, instituteId, status, studentId, userId);
+    
+    // Create the admission with studentId and userId
+    AdmissionModel createdAdmission = admissionService.createAdmission(admission);
+    
+    return ResponseEntity.ok(createdAdmission);
+}
 
-        // Create the admission with studentId and userId
-        AdmissionModel createdAdmission = admissionService.createAdmission(admission, studentId, userId);
-        return ResponseEntity.ok(createdAdmission);
 
 
-    }
+        
+
+
+
+
+
+
 
     // Edit an admission by ID
     @PutMapping("/admin/editAdmission/{admissionId}")
