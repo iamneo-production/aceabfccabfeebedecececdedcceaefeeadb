db0d14e90183dff98b5398d32a050f398613d1cc
Iamneo <techadmin@iamneo.ai>
2023-09-17 03:43:46 +0000

    Backup Commit

--- reactapp/src/components/Auth/Signup/Signup.js
+++ reactapp/src/components/Auth/Signup/Signup.js
@@ -1,3 +1,249 @@
+// import React, { useState } from 'react';
+// import Avatar from '@mui/material/Avatar';
+// import Button from '@mui/material/Button';
+// import CssBaseline from '@mui/material/CssBaseline';
+// import TextField from '@mui/material/TextField';
+// import Grid from '@mui/material/Grid';
+// import Box from '@mui/material/Box';
+// import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
+// import Typography from '@mui/material/Typography';
+// import Container from '@mui/material/Container';
+// import { Select, InputLabel, MenuItem, FormControl } from '@mui/material';
+// import { createTheme, ThemeProvider } from '@mui/material/styles';
+// import { Link, useNavigate } from 'react-router-dom';
+// import axios from 'axios';
+
+// const defaultTheme = createTheme();
+
+// export default function SignUp() {
+//   const [roleError, setRoleError] = useState(false);
+//   const [emailError, setEmailError] = useState(false);
+//   const [userNameError, setUserNameError] = useState(false);
+//   const [mobileNumberError, setMobileNumberError] = useState(false);
+//   const [passwordError, setPasswordError] = useState(false);
+
+//   const validateMobile = (mobile) => {
+//     const mobileRegex = /^(\+[0-9]{1,3})?([0-9]{10})$/;
+//     return mobileRegex.test(mobile);
+//   };
+
+//   const validatePasswords = (password, reEnterPassword) => {
+//     return password === reEnterPassword && password !== '';
+//   };
+
+//   const validateEmail = (email) => {
+//     const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+//     return emailRegex.test(email);
+//   };
+//   const navigate = useNavigate();
+
+//   const handleInputChange = (event) => {
+//     const fieldName = event.target.name;
+//     switch (fieldName) {
+//       case 'userRole':
+//         setRoleError(false);
+//         break;
+//       case 'email':
+//         setEmailError(false);
+//         break;
+//       case 'username':
+//         setUserNameError(false);
+//         break;
+//       case 'mobileNumber':
+//         setMobileNumberError(false);
+//         break;
+//       case 'password':
+//         setPasswordError(false);
+//         break;
+//       case 'confirmPassword':
+//         setPasswordError(false);
+//         break;
+//       default:
+//         break;
+//     }
+//   };
+
+//   const handleSubmit = (event) => {
+//     event.preventDefault();
+//     const data = new FormData(event.currentTarget);
+//     const signUpData = {};
+
+//     if (!data.get('userRole')) {
+//       setRoleError(true);
+//       return;
+//     }
+
+//     if (!validateEmail(data.get('email'))) {
+//       setEmailError(true);
+//       return;
+//     }
+
+//     if (!data.get('username')) {
+//       setUserNameError(true);
+//       return;
+//     }
+
+//     if (!validateMobile(data.get('mobileNumber'))) {
+//       setMobileNumberError(true);
+//       return;
+//     }
+
+//     if (!validatePasswords(data.get('password'), data.get('confirmPassword'))) {
+//       setPasswordError(true);
+//       return;
+//     }
+
+//     for (const [key, value] of data.entries()) {
+//       signUpData[key] = value;
+//     }
+
+//     // Determine the API endpoint based on the selected role
+//     let apiEndpoint = '';
+//     if (signUpData.userRole === 'admin') {
+//       apiEndpoint = 'https://8080-aceabfccabfeebedecececdedcceaefeeadb.premiumproject.examly.io/admin/signup';
+//     } else if (signUpData.userRole === 'user') {
+//       apiEndpoint = 'https://8080-aceabfccabfeebedecececdedcceaefeeadb.premiumproject.examly.io/user/signup';
+//     }
+
+//     axios
+//       .post(apiEndpoint, signUpData)
+//       .then((response) => {
+//         console.log(response.data); // Handle success
+//         navigate(`/Login`);
+//       })
+//       .catch((error) => {
+//         console.error(error); // Handle error
+//       });
+//   };
+
+//   return (
+//     <ThemeProvider theme={defaultTheme}>
+//       <Container component="main" maxWidth="xs">
+//         <CssBaseline />
+//         <Box
+//           sx={{
+//             marginTop: 8,
+//             display: 'flex',
+//             flexDirection: 'column',
+//             alignItems: 'center',
+//             border: '1px solid #ccc', // Add border
+//             borderRadius: '8px', // Rounded corners
+//             padding: '16px', // Add padding
+//           }}
+//         >
+//           <Avatar sx={{ m: 1, bgcolor: 'secondary.main' }}>
+//             <LockOutlinedIcon />
+//           </Avatar>
+//           <Typography component="h1" variant="h5">
+//             Sign up
+//           </Typography>
+//           <Box component="form" noValidate onSubmit={handleSubmit} sx={{ mt: 3 }}>
+//             <Grid container spacing={2}>
+//               <Grid item xs={12}>
+//                 <FormControl fullWidth>
+//                   <InputLabel id="demo-simple-select-label">Select Admin or User</InputLabel>
+//                   <Select
+//                     labelId="demo-simple-select-label"
+//                     id="demo-simple-select"
+//                     label="Select Admin or User"
+//                     name="userRole"
+//                     error={roleError}
+//                     helperText={roleError ? 'Please select a role' : ''}
+//                     onChange={handleInputChange} // Reset error on change
+//                   >
+//                     <MenuItem value={"admin"}>Admin</MenuItem>
+//                     <MenuItem value={"user"}>User</MenuItem>
+//                   </Select>
+//                 </FormControl>
+//               </Grid>
+//               <Grid item xs={12}>
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   id="email"
+//                   label="Email Address"
+//                   name="email"
+//                   autoComplete="email"
+//                   error={emailError}
+//                   helperText={emailError ? 'Please enter a valid email' : ''}
+//                   onChange={handleInputChange} // Reset error on change
+//                 />
+//               </Grid>
+//               <Grid item xs={12}>
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   name="username"
+//                   label="Enter Username"
+//                   id="username"
+//                   error={userNameError}
+//                   helperText={userNameError ? 'Please enter a username' : ''}
+//                   onChange={handleInputChange}
+//                 />
+//               </Grid>
+//               <Grid item xs={12}>
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   name="mobileNumber"
+//                   label="Enter Mobile Number"
+//                   id="mobileNumber"
+//                   autoComplete="mobileNumber"
+//                   error={mobileNumberError}
+//                   helperText={mobileNumberError ? 'Please enter a valid mobile number' : ''}
+//                   onChange={handleInputChange} // Reset error on change
+//                 />
+//               </Grid>
+//               <Grid item xs={12}>
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   name="password"
+//                   label="Password"
+//                   type="password"
+//                   id="password"
+//                   autoComplete="new-password"
+//                   onChange={handleInputChange} // Reset error on change
+//                 />
+//               </Grid>
+//               <Grid item xs={12}>
+//                 <TextField
+//                   required
+//                   fullWidth
+//                   name="confirmPassword"
+//                   label="Confirm Password"
+//                   type="password"
+//                   id="confirmPassword"
+//                   autoComplete="confirm password"
+//                   error={passwordError}
+//                   helperText={passwordError ? 'Passwords do not match' : ''}
+//                   onChange={handleInputChange} // Reset error on change
+//                 />
+//               </Grid>
+//             </Grid>
+//             <Button
+//               type="submit"
+//               id="submitButton"
+//               fullWidth
+//               variant="contained"
+//               sx={{ mt: 3, mb: 2 }}
+//             >
+//               Submit
+//             </Button>
+//             <Grid container justifyContent="flex-end">
+//               <Grid item>
+//                 <span>Already a user ? </span>
+//                 <Link to="/Login" variant="body2">
+//                   Sign in
+//                 </Link>
+//               </Grid>
+//             </Grid>
+//           </Box>
+//         </Box>
+//       </Container>
+//     </ThemeProvider>
+//   );
+// }
 import React, { useState } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
@@ -12,6 +258,8 @@ import { Select, InputLabel, MenuItem, FormControl } from '@mui/material';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { Link, useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const defaultTheme = createTheme();
 
@@ -108,8 +356,14 @@ export default function SignUp() {
     axios
       .post(apiEndpoint, signUpData)
       .then((response) => {
-        console.log(response.data); // Handle success
-        navigate(`/Login`);
+        if (response.data === 'Email already exists') {
+          // Show a red toast for error
+          toast.error(response.data, { autoClose: 3000 });
+        } else {
+          // Show a green toast for success
+          toast.success('Sign up successful', { autoClose: 3000 });
+          navigate(`/Login`);
+        }
       })
       .catch((error) => {
         console.error(error); // Handle error
@@ -240,6 +494,7 @@ export default function SignUp() {
             </Grid>
           </Box>
         </Box>
+        <ToastContainer />
       </Container>
     </ThemeProvider>
   );
