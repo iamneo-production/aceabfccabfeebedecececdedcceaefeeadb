f210e102268c0ebc2882af904c736aaa25970b49
Iamneo <techadmin@iamneo.ai>
2023-09-18 09:52:42 +0000

    Backup Commit

--- springapp/src/main/java/com/examly/springapp/service/CourseService.java
+++ springapp/src/main/java/com/examly/springapp/service/CourseService.java
@@ -4,19 +4,22 @@ import com.examly.springapp.model.CourseModel;
 
 import java.util.Optional;
 import java.util.*;
+
 public interface CourseService {
     CourseModel createCourse(CourseModel course);
-    Optional<CourseModel> getCourseById(int courseId);
-    List<CourseModel> getAllCourses();
-    String getCourseNameById(int courseId);
-    CourseModel createCourseWithInstitute(int instituteId, CourseModel course);
-   
 
+    Optional<CourseModel> getCourseById(int courseId);
+
+    List<CourseModel> getAllCourses();
+
+    String getCourseNameById(int courseId);
+
+    CourseModel createCourseWithInstitute(int instituteId, CourseModel course);
 
     List<CourseModel> getCoursesByInstituteInstituteId(int instituteId);
 
-
     CourseModel updateCourse(int courseId, CourseModel updatedCourse);
+
     void deleteCourse(int courseId);
     // Add custom service methods if needed
 }
