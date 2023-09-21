0ce026c4cc35885669492b6b34756d360db98528
Iamneo <techadmin@iamneo.ai>
2023-09-21 02:34:56 +0000

    Backup Commit

--- reactapp/src/components/AdminAppBar.js
+++ reactapp/src/components/AdminAppBar.js
@@ -3,33 +3,49 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Button from '@mui/material/Button';
-import { useNavigate ,useParams } from 'react-router-dom';
+import { useNavigate, useParams, useLocation } from 'react-router-dom';
 
 const AdminAppBar = (props) => {
   const navigate = useNavigate();
+  const { id } = useParams();
+  const location = useLocation();
+
+  // Function to check if a button is selected based on the current route
+  const isButtonSelected = (route) => {
+    return location.pathname.includes(route);
+  };
+
   const Logout = () => {
     navigate(`/Login`);
   };
-  const params =  useParams();
 
   return (
     <Box sx={{ flexGrow: 1 }}>
- 
       <AppBar position="static">
         <Toolbar sx={{ justifyContent: 'space-between' }}>
           {/* Button Container for Centered Buttons */}
           <div style={{ display: 'flex', alignItems: 'center', marginLeft: 'auto', marginRight: 'auto' }}>
-            <Button id= "adminInstitute" color="inherit" onClick={() => navigate(`/AdminHomePage/UniversityList/${props.id}`)}>
+            <Button
+              id="adminInstitute"
+              color={isButtonSelected(`/AdminHomePage/UniversityList/${props.id}`) ? 'secondary' : 'inherit'}
+              onClick={() => navigate(`/AdminHomePage/UniversityList/${props.id}`)}
+            >
               Institute
-             </Button>
-            <Button id= "adminAllCourses" color="inherit" onClick={() => navigate(`/AdminHomePage/AllCourses/${props.id}`)}>
+            </Button>
+            <Button
+              id="adminAllCourses"
+              color={isButtonSelected(`/AdminHomePage/AllCourses/${props.id}`) ? 'secondary' : 'inherit'}
+              onClick={() => navigate(`/AdminHomePage/AllCourses/${props.id}`)}
+            >
               All Courses
-            </Button> 
-            <Button id="adminStudents" color="inherit" onClick={() => navigate(`/AdminHomePage/Students/${props.id}`)}>
+            </Button>
+            <Button
+              id="adminStudents"
+              color={isButtonSelected(`/AdminHomePage/Students/${props.id}`) ? 'secondary' : 'inherit'}
+              onClick={() => navigate(`/AdminHomePage/Students/${props.id}`)}
+            >
               Students
             </Button>
-
-            
           </div>
 
           {/* Logout Button */}
